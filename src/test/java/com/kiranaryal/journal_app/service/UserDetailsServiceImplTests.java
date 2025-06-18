c189e50ea6734517435a6203f256f7b107afa62c
Kiran Aryal <kiranaryal1997@gmail.com>
2025-06-18 16:57:46 +0545

    commented test

--- src/test/java/com/kiranaryal/journal_app/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/kiranaryal/journal_app/service/UserDetailsServiceImplTests.java
@@ -1,45 +1,45 @@
-package com.kiranaryal.journal_app.service;
-
-import com.kiranaryal.journal_app.entity.User;
-import com.kiranaryal.journal_app.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.*;
-
-//@SpringBootTest
+//package com.kiranaryal.journal_app.service;
+//
+//import com.kiranaryal.journal_app.entity.User;
+//import com.kiranaryal.journal_app.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.ArgumentMatchers;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.ArrayList;
+//
+//import static org.mockito.Mockito.*;
+//
+////@SpringBootTest
+////public class UserDetailsServiceImplTests {
+//
+//
 //public class UserDetailsServiceImplTests {
-
-
-public class UserDetailsServiceImplTests {
-
-    @InjectMocks
-    private UserDetailsServiceImpl userDetailsService;
-
-    @Mock
-    private UserRepository userRepository;
-
-    //before any test run this method
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    void loadUserByUsernameTest() {
-        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("asmita").password("al;skjdf").roles(new ArrayList<>()).build());
-        UserDetails user = userDetailsService.loadUserByUsername("asmita");
-        Assertions.assertNotNull(user);
-    }
-}
+//
+//    @InjectMocks
+//    private UserDetailsServiceImpl userDetailsService;
+//
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    //before any test run this method
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.initMocks(this);
+//    }
+//
+//    @Test
+//    void loadUserByUsernameTest() {
+//        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("asmita").password("al;skjdf").roles(new ArrayList<>()).build());
+//        UserDetails user = userDetailsService.loadUserByUsername("asmita");
+//        Assertions.assertNotNull(user);
+//    }
+//}
--- src/test/java/com/kiranaryal/journal_app/service/UserServiceTests.java
+++ src/test/java/com/kiranaryal/journal_app/service/UserServiceTests.java
@@ -1,64 +1,64 @@
-package com.kiranaryal.journal_app.service;
-
-
-import com.kiranaryal.journal_app.entity.User;
-import com.kiranaryal.journal_app.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class UserServiceTests {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserService userService;
-
-    //example of test
-    @Test
-    public void testAdd() {
-        Assertions.assertEquals(15, 5 + 5 + 5);
-    }
-
-    @Test
-    public void testFindByUsername() {
-        Assertions.assertNotNull(userRepository.findByUsername("kiran"));
-    }
-
-    @Test
-    public void testJournalEntriesOfUser() {
-        User user = userRepository.findByUsername("kiran");
-        Assertions.assertTrue(!user.getJournals().isEmpty());
-    }
-
-    //example of parameterized test
-    @ParameterizedTest
-    @CsvSource({"1,1,2", "2,10,12", "3,3,6"})
-    public void test(int a, int b, int expected) {
-        Assertions.assertEquals(expected, a + b);
-    }
-
-//    @ParameterizedTest
-//    @ArgumentsSource(UserArgumentsProvider.class)
-//    public void testSaveNewUser(User user) {
-//        Assertions.assertTrue(userService.saveNewUser(user), "failed for: " + user.getUsername());
+//package com.kiranaryal.journal_app.service;
+//
+//
+//import com.kiranaryal.journal_app.entity.User;
+//import com.kiranaryal.journal_app.repository.UserRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ArgumentsSource;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class UserServiceTests {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    //example of test
+//    @Test
+//    public void testAdd() {
+//        Assertions.assertEquals(15, 5 + 5 + 5);
 //    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            "kiran",
-            "asmit",
-            "asmita",
-            "adarsha"
-    })
-    public void testJournalEntriesOfUser(String name) {
-        Assertions.assertNotNull(userRepository.findByUsername(name), "failed for: " + name);
-    }
-}
+//
+//    @Test
+//    public void testFindByUsername() {
+//        Assertions.assertNotNull(userRepository.findByUsername("kiran"));
+//    }
+//
+//    @Test
+//    public void testJournalEntriesOfUser() {
+//        User user = userRepository.findByUsername("kiran");
+//        Assertions.assertTrue(!user.getJournals().isEmpty());
+//    }
+//
+//    //example of parameterized test
+//    @ParameterizedTest
+//    @CsvSource({"1,1,2", "2,10,12", "3,3,6"})
+//    public void test(int a, int b, int expected) {
+//        Assertions.assertEquals(expected, a + b);
+//    }
+//
+////    @ParameterizedTest
+////    @ArgumentsSource(UserArgumentsProvider.class)
+////    public void testSaveNewUser(User user) {
+////        Assertions.assertTrue(userService.saveNewUser(user), "failed for: " + user.getUsername());
+////    }
+//
+//    @ParameterizedTest
+//    @ValueSource(strings = {
+//            "kiran",
+//            "asmit",
+//            "asmita",
+//            "adarsha"
+//    })
+//    public void testJournalEntriesOfUser(String name) {
+//        Assertions.assertNotNull(userRepository.findByUsername(name), "failed for: " + name);
+//    }
+//}
