c5231e55bbf276ecf9f63fe92ebef28b2a9158a2
Kiran Aryal <kiranaryal1997@gmail.com>
2025-06-18 17:03:30 +0545

    updated

--- src/test/java/com/kiranaryal/journal_app/service/UserArgumentsProvider.java
+++ src/test/java/com/kiranaryal/journal_app/service/UserArgumentsProvider.java
@@ -1,19 +1,19 @@
-//package com.kiranaryal.journal_app.service;
-//
-//import com.kiranaryal.journal_app.entity.User;
-//import org.junit.jupiter.api.extension.ExtensionContext;
-//import org.junit.jupiter.params.provider.Arguments;
-//import org.junit.jupiter.params.provider.ArgumentsProvider;
-//
-//import java.util.stream.Stream;
-//
-//public class UserArgumentsProvider implements ArgumentsProvider {
-//
-//    @Override
-//    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-//        return Stream.of(
-//                Arguments.of(User.builder().username("adarsha").password("adarsha").build()),
-//                Arguments.of(User.builder().username("hemlal").password("").build())
-//        );
-//    }
-//}
+package com.kiranaryal.journal_app.service;
+
+import com.kiranaryal.journal_app.entity.User;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+public class UserArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of(User.builder().username("adarsha").password("adarsha").build()),
+                Arguments.of(User.builder().username("hemlal").password("").build())
+        );
+    }
+}
